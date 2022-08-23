19109333a8b502996dd47f79a0f60ffceac27305
Vitor Almeida <almeida.f.vitor@gmail.com>
2022-08-23 11:16:51 -0300

    working mixed f16 and slanted lr

--- lawclassification/train/deep_train.py
+++ lawclassification/train/deep_train.py
@@ -4,11 +4,15 @@ import gc
 from models.deep_models import deep_models
 from utils.helper_funs import print_params_terminal,print_step_log,print_batch_log
 from utils.deep_metrics import deep_metrics, execute_metrics_type, compute_metrics_type, reset_all
+import mlflow
 
 class deep_train():
     """
     Train diferent deep models
     """
+
+    #mlflow ui --backend-store-uri sqlite:///mlflow.db
+
     def __init__(self,experiment):
 
         super(deep_train, self).__init__()
@@ -33,21 +37,28 @@ class deep_train():
     def train_loop(self,epoch_i):
 
         self.model.model.train()
+        scaler = torch.cuda.amp.GradScaler(enabled=True)
 
         for idx,batch in enumerate(self.model.train_dataloader):
             self.model.optimizer.zero_grad()
             batch = {k: v.to(self.model.device) for k, v in batch.items()}
-            outputs = self.model.model(**batch)
-            loss = outputs.loss 
-            loss.backward()
+
+            #mixed precision training:
+            with torch.autocast(device_type=self.model.device.type, dtype=torch.float16, enabled=True):
+                outputs = self.model.model(**batch)
+                loss = outputs.loss 
+
+            scaler.scale(loss).backward() #loss.backward()
+            scaler.unscale_(self.model.optimizer)
             torch.nn.utils.clip_grad_norm_(self.model.model.parameters(), 1.0)
-            self.model.optimizer.step()
-            self.model.scheduler.step() #diferença aqui entre o tutorial do pytorch ?
+            
+            scaler.step(self.model.optimizer) #self.model.optimizer.step()
+            scaler.update()
+            
+            self.model.scheduler.step()
 
             metricsResults = execute_metrics_type(self.metrics.metricDic['Train'],outputs.logits, batch['labels'].int())
-
             print_step_log(idx,epoch_i,self.model,metricsResults['Batch']) #fica dando o resultado de acuracia do step idx, 'nao eh acumulado'
-
             self.progress_bar.update(1)
 
         return None
@@ -59,7 +70,9 @@ class deep_train():
         with torch.no_grad():
             for batch in self.model.test_dataloader:
                 batch = {k: v.to(self.model.device) for k, v in batch.items()}
-                outputs = self.model.model(**batch)
+
+                with torch.autocast(device_type=self.model.device.type, dtype=torch.float16, enabled=True):
+                    outputs = self.model.model(**batch)
 
                 #lexGlue tem q fazer um if self.model.dataname == 'unfair-tos', cat([1] ou [0] no começo do vetor se a label for [0,0,0..0])
                 #problema q talvez vai bugar a definição das métricas
@@ -75,7 +88,9 @@ class deep_train():
         with torch.no_grad():
             for batch in self.model.val_dataloader:
                 batch = {k: v.to(self.model.device) for k, v in batch.items()}
-                outputs = self.model.model(**batch)
+
+                with torch.autocast(device_type=self.model.device.type, dtype=torch.float16, enabled=True):
+                    outputs = self.model.model(**batch)
 
                 metricsResults = execute_metrics_type(self.metrics.metricDic['Val'],outputs.logits, batch['labels'].int())
 
@@ -96,9 +111,6 @@ class deep_train():
             print('------------Test results:-----------------------')
             print_batch_log(epoch_i,self.model,testTmpResult)
             testTmpResult = compute_metrics_type(self.metrics.metricDic['Test']['Batch'],action='reset')
-
-        
-        
 
         self.val_loop()
         valTmpResult = compute_metrics_type(self.metrics.metricDic['Val']['Batch'],action='compute')
