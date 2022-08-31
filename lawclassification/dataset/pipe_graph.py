0b0ab1bb20dea4c5faa39652c0c8c3ec881e7f06
Vitor Almeida <almeida.f.vitor@gmail.com>
2022-08-31 12:02:54 -0300

    faster pmi calculation

--- lawclassification/dataset/pipe_graph.py
+++ lawclassification/dataset/pipe_graph.py
@@ -10,6 +10,38 @@ from tqdm.auto import tqdm
 import spacy
 import gc
 
+def _calc_pmi(rdx:int,
+              cdx:int,
+              npAgg:np.array,
+              rowIndicesList:List,
+              totalWindows:np.int16) -> np.float16:
+
+    if cdx > rdx:
+        return None
+    if cdx == rdx:
+        return 1
+
+    rowSumTT = np.sum(npAgg[rowIndicesList[rdx]])
+    colSumTT = np.sum(npAgg[rowIndicesList[cdx]])
+
+    indicesRowandCol = list(np.intersect1d(rowIndicesList[rdx],rowIndicesList[cdx]))
+    rowColSumTT = np.sum(npAgg[indicesRowandCol])
+
+    pRow = rowSumTT / totalWindows
+    pCol = colSumTT / totalWindows
+    pRowCol = rowColSumTT / totalWindows
+    
+    if pRowCol == 0 or (pRow*pCol == 0):
+        #avoid math errors
+        pmi = 0
+    else:
+        pmi = math.log(pRowCol / (pRow*pCol),2)
+
+    if pmi > 0:
+        return pmi
+    else:
+        return None
+
 NLP = spacy.load('en_core_web_lg')
 
 def _tokenizer(text:str) -> str:
@@ -24,24 +56,18 @@ def _tokenizer(text:str) -> str:
 
     return newText
 
-def _load_csv(fileName: str,path: str,max_rows: int) -> pd.DataFrame:
+def _load_csv(path: str,max_rows: int) -> pd.DataFrame:
 
-    df = pd.read_csv(os.path.join(ROOT_DIR,'data',path,'interm',fileName,fileName+'.csv'))
+    dfTrain = pd.read_csv(os.path.join(ROOT_DIR,'data',path,'interm','train','train.csv'))
+    dfTest = pd.read_csv(os.path.join(ROOT_DIR,'data',path,'interm','train','train.csv'))
+    dfVal = pd.read_csv(os.path.join(ROOT_DIR,'data',path,'interm','train','train.csv'))
+
+    df = pd.concat([dfTrain,dfTest,dfVal])
+    #df = df.sample(frac=1)
+
     df = df.head(max_rows)
 
     return df
-
-def _getExactMatch(arr:np.array,word:str) -> List:
-
-    indices = []
-
-    #indices2 = [idx for idx,ngrams in enumerate(arr) if re.search(r'\b' + word + r'\b', ngrams)]
-
-    for idx,ngrams in enumerate(arr):
-        if re.search(r'\b' + word + r'\b', ngrams):
-            indices.append(idx)
-
-    return indices
 
 def _aggNpArray(arr:np.array) -> np.array:
 
@@ -73,7 +99,7 @@ def create_graph(path: str,maxRows: int, windowSize:int) -> None:
     n     | something something ...
     '''
 
-    count_vector = CountVectorizer(#tokenizer = _tokenizer,
+    count_vector = CountVectorizer(tokenizer = _tokenizer,
                                 #stop_words = 'english', 
                                 #sublinear_tf = True, 
                                 ngram_range=(windowSize,windowSize),
@@ -87,7 +113,7 @@ def create_graph(path: str,maxRows: int, windowSize:int) -> None:
                                 max_features = None, #numero maximo de features
                                 vocabulary = None)
 
-    tfidf_vector = TfidfVectorizer(#tokenizer = _tokenizer,
+    tfidf_vector = TfidfVectorizer(tokenizer = _tokenizer,
                                 #stop_words = 'english', 
                                 #sublinear_tf = True, 
                                 ngram_range=(1,1),
@@ -102,108 +128,82 @@ def create_graph(path: str,maxRows: int, windowSize:int) -> None:
                                 vocabulary = None)
 
 
-    dfTrain = _load_csv('train',path,maxRows)
+    dfTable = _load_csv(path,maxRows)
 
-    countMatrix = count_vector.fit_transform(dfTrain['text'])
-    tfidf_matrix = tfidf_vector.fit_transform(dfTrain['text'])
-    labels = dfTrain['labels'].to_frame()
+    countMatrix = count_vector.fit_transform(dfTable['text'])
+    tfidf_matrix = tfidf_vector.fit_transform(dfTable['text'])
+    labels = dfTable['labels'].to_frame()
     labels['src'] = labels.index
     labels['src'] = labels['src'].apply(lambda row: 'doc_'+str(row))
 
-    del dfTrain
+    del dfTable
     gc.collect()
 
     vocabularyVec = tfidf_vector.get_feature_names_out().astype('U')
     vocabularyWindow = count_vector.get_feature_names_out().astype('U')
 
     dfCount = pd.DataFrame.sparse.from_spmatrix(countMatrix, columns=vocabularyWindow)
-    dfCount = pd.melt(dfCount,value_name='__value__',var_name='__variable__')
+
+    dfMeltCount = []
+    for col in dfCount.columns:
+        tmpDf = pd.melt(dfCount[col].to_frame(),value_name='__value__',var_name='__variable__') #da pra fzer por grupos
+        tmpDf['__value__'] = tmpDf['__value__'].astype(np.int16)
+        tmpDf['__variable__'] = tmpDf['__variable__'].astype(str)
+        tmpDf = tmpDf[tmpDf['__value__']!=0]
+        dfMeltCount.append(tmpDf)
+
+    dfCount = pd.concat(dfMeltCount)
     dfCount = dfCount.groupby(by=['__variable__']).sum()
     dfCount.reset_index(inplace=True)
     
     dfDocWord = pd.DataFrame.sparse.from_spmatrix(tfidf_matrix, columns=vocabularyVec)
     dfDocWord['docId'] = dfDocWord.index
-    dfDocWord = pd.melt(dfDocWord,id_vars=['docId'],value_name='__value__',var_name='__variable__')
-    dfDocWord.set_index(['docId','__variable__'],drop=True,inplace=True)
-    dfDocWord = dfDocWord[dfDocWord['__value__']>0]
-    dfDocWord.reset_index(inplace=True)
+
+    dfMeltCount = []
+    for col in dfDocWord.columns[:-1]:
+        tmpDf = pd.melt(dfDocWord[[col,'docId']],id_vars=['docId'],value_name='__value__',var_name='__variable__')
+        tmpDf = tmpDf[tmpDf['__value__']!=0]
+        tmpDf.set_index(['docId','__variable__'],drop=True,inplace=True)
+        tmpDf.reset_index(inplace=True)
+        dfMeltCount.append(tmpDf)
+
+    dfDocWord = pd.concat(dfMeltCount)
+    del dfMeltCount
+    gc.collect()
+
+    dfDocWord.reset_index(inplace=True,drop=True)
     dfDocWord['docId'] = dfDocWord['docId'].apply(lambda row: 'doc_'+str(row))
     dfDocWord.rename(columns={'docId':'src','__variable__':'tgt','__value__':'weight'},inplace=True)
     dfDocWord = dfDocWord.merge(labels, how='left', on='src')
 
-    #structure to search for stuff from a sparse matrix
-    #for gId,ngram in enumerate(vocabularyWindow):
-    #    tmpGram = np.array(ngram, dtype='U')
-    #    tmpGram = np.expand_dims(tmpGram,axis=0)
-
-        #if gId > 100:
-        #    break
-
-        #for docId,doc in enumerate(count_matrix):
-
-        #    itemIdx = np.where(doc.indices == gId)
-        #    itemIdx = itemIdx[0][0] if len(itemIdx[0]) > 0 else None
-        #    if itemIdx != None:
-        #        count = doc.data[itemIdx]
-        #        newGram = np.concatenate((tmpGram,np.array([int(docId),int(count),int(gId)])),axis=0)
-
-                #finalGram.append(np.expand_dims(newGram,axis=0))
-
     npAggText = dfCount['__variable__'].to_numpy(dtype=str)
-    #npAggText = npAggText[:100]
     npAggCount = dfCount['__value__'].to_numpy(dtype=np.int16)
 
     del dfCount
     gc.collect()
 
+    rowIndicesList = []
+    for wordRow in vocabularyVec:
+        indicesRow = [idx for idx,ngrams in enumerate(npAggText) if re.search(r'\b' + wordRow + r'\b', ngrams)]
+        rowIndicesList.append(indicesRow)
+
     totalWindows = np.sum(npAggCount)
-
     progressBar = tqdm(range(int((len(vocabularyVec)**2)/2)))
-
-    vocLoopRow = vocabularyVec.copy()
-    vocLoopCol = vocabularyVec.copy()
-
-    print(len(vocLoopRow))
-    print(len(npAggCount))
     wordWordList = []
-    for wordRow in vocLoopRow:
-        for wordCol in vocLoopCol:
+    for rdx,wordRow in enumerate(vocabularyVec):
+        for cdx,wordCol in enumerate(vocabularyVec):
 
-            #indicesRow = [idx for idx,ngrams in enumerate(npAggText) if re.search(r'\b' + wordRow + r'\b', ngrams)]
-            indicesRow = [1]#list(np.flatnonzero(np.core.defchararray.find(npAggText,wordRow)!=-1))
-            #indicesRow = _getExactMatch(npAggText,wordRow)
-            rowSumTT = np.sum(npAggCount[indicesRow])
-
-            #indicesCol = [idx for idx,ngrams in enumerate(npAggText) if re.search(r'\b' + wordCol + r'\b', ngrams)]
-            indicesCol = [1]#list(np.flatnonzero(np.core.defchararray.find(npAggText,wordCol)!=-1))
-            #indicesCol = _getExactMatch(npAggText,wordCol)
-            colSumTT = np.sum(npAggCount[indicesCol])
-
-            indicesRowandCol = list(np.intersect1d(indicesRow,indicesCol))
-            rowColSumTT = np.sum(npAggCount[indicesRowandCol])
-
-            pRow = rowSumTT / totalWindows
-            pCol = colSumTT / totalWindows
-            pRowCol = rowColSumTT / totalWindows
+            pmi = _calc_pmi(rdx,cdx,npAggCount,rowIndicesList,totalWindows)
             
-            if pRowCol == 0 or (pRow*pCol == 0):
-                pmi = 0
-            else:
-                pmi = math.log(pRowCol / (pRow*pCol),2)
-
-            #try:
-                #pmi = math.log(pRowCol / (pRowTpCol),2)
-            #except:
-                #pmi = 0
-            
-            if wordRow == wordCol:
-                wordWordList.append([wordRow,wordCol,1])
-            elif pmi > 0:
-                wordWordList.append([wordRow,wordCol,pmi])
+            if pmi != None:
+                wordWordList.append([wordRow,wordCol,pmi,999])
 
             progressBar.update(1)
 
-        #getting only a->b , not a->b AND b->a
-        vocLoopCol = vocLoopCol[vocLoopCol != wordRow]
+    dfWordWord = pd.DataFrame(wordWordList,columns=['src','tgt','weight','labels'])
+
+    dfGraph = pd.concat([dfWordWord,dfDocWord])
+
+    dfGraph.to_csv(os.path.join(ROOT_DIR,'data',path,'interm','graph.csv'),index=False)
     
     return None
