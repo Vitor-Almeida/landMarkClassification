0ab71e435d6b3bf7fb84b9ae1f9b1874e2d167e8
Vitor Almeida <almeida.f.vitor@gmail.com>
2022-09-20 16:58:36 -0300

    gcn training in pipeline

--- lawclassification/main.py
+++ lawclassification/main.py
@@ -1,5 +1,6 @@
 from utils.helper_funs import read_experiments
 from train.deep_train import deep_train
+from train.gcn_train import gcn_train
 import models.tfidf_models as boost
 import gc
 import mlflow
@@ -11,6 +12,7 @@ def main():
     mlflow.set_tracking_uri('sqlite:///mlflow.db')
     expDicDeep = read_experiments('run_experiments_deep.csv','deep_learning')
     expDicBoost = read_experiments('run_experiments_boost.csv','boost')
+    expDicGCN = read_experiments('run_experiments_gcn.csv','gcn_learning')
     
     #mlflow.delete_experiment(experiment_id=3)
     #mlflow gc --backend-store-uri sqlite:///mlflow.db
@@ -37,6 +39,7 @@ def main():
         del train
         gc.collect()
 
+    #xgboost training
     for idx,experiment in enumerate(expDicBoost):
         print(f'begin of boost experiment: {idx+1}/{len(expDicBoost)}')
         print(f'Olhar resultados no mlflow !')
@@ -52,6 +55,22 @@ def main():
             boostExp.train()
 
         del boostExp
+        gc.collect()
+
+    #gcn training
+    for idx,experiment in enumerate(expDicGCN):
+        print(f'begin of gcn experiment: {idx+1}/{len(expDicBoost)}')
+        print(f'Olhar resultados no mlflow !')
+
+        #mlflow.set_experiment(experiment['dataname']+experiment['model_name'])
+
+        gcnExp = gcn_train(experiment)
+        gcnExp.fit_and_eval()
+
+        #mlflow.xgboost.autolog(log_input_examples=False,log_model_signatures=False,log_models=False)
+        #with mlflow.start_run(run_name=experiment['descripton']):
+
+        del gcnExp
         gc.collect()
 
     return None
